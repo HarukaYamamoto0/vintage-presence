41ae09a840c72564e9e5effc84145485cb371e5e
HarukaYamamoto0 <harukayamamotodev@gmail.com>
2025-12-03 07:42:40 -0300

    Initial commit

---
+++ src/VintagePresenceMod.cs
@@ -0,0 +1,22 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+
+namespace VintagePresence;
+
+// ReSharper disable once UnusedType.Global
+public class VintagePresenceMod : ModSystem
+{
+    private const string ModLogPrefix = "[VintagePresence]";
+    private const string ConfigFileName = "ForgeCordConfig.json";
+
+    public override void StartClientSide(ICoreClientAPI api)
+    {
+        base.StartClientSide(api);
+        api.Logger.Event($"{ModLogPrefix} loaded!");
+    }
+
+    public override bool ShouldLoad(EnumAppSide forSide)
+    {
+        return forSide == EnumAppSide.Client;
+    }
+}
